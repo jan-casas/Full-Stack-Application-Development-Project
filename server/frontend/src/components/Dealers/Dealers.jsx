f215c86f4ca1507b33035e982c2ff6aa38a10ac9
Jan <casasvil@protonmail.com>
2025-07-05 21:33:07 +0200

    feat: initial commit with full capstone project

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -21,7 +21,7 @@ const Dealers = () => {
     });
     const retobj = await res.json();
     if(retobj.status === 200) {
-      let state_dealers = Array.from(retobj.dealers)
+      let state_dealers = Array.from(retobj.dealers || [])
       setDealersList(state_dealers)
     }
   }
@@ -32,7 +32,7 @@ const Dealers = () => {
     });
     const retobj = await res.json();
     if(retobj.status === 200) {
-      let all_dealers = Array.from(retobj.dealers)
+      let all_dealers = Array.from(retobj.dealers || [])
       let states = [];
       all_dealers.forEach((dealer)=>{
         states.push(dealer.state)
